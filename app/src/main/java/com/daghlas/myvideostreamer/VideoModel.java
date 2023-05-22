d1fdcaff5907abac50216cabb954e412000190fb
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-05-22 11:10:41 +0300

    initial commit

---
+++ app/src/main/java/com/daghlas/myvideostreamer/VideoModel.java
@@ -0,0 +1,50 @@
+package com.daghlas.myvideostreamer;
+
+public class VideoModel {
+
+    String title;
+    String description;
+    String author;
+    String videoUri;
+    String imageUri;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getVideoUri() {
+        return videoUri;
+    }
+
+    public void setVideoUri(String videoUri) {
+        this.videoUri = videoUri;
+    }
+
+    public String getImageUri() {
+        return imageUri;
+    }
+
+    public void setImageUri(String imageUri) {
+        this.imageUri = imageUri;
+    }
+}
