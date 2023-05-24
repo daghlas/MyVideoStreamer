c1fe0a797941c84de732b3d27c92be22f64f4464
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-05-24 10:05:43 +0300

    initial commit

--- app/src/main/java/com/daghlas/myvideostreamer/VideoAdapter.java
+++ app/src/main/java/com/daghlas/myvideostreamer/VideoAdapter.java
@@ -1,13 +1,32 @@
 package com.daghlas.myvideostreamer;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
 public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.MyViewHolder> {
+
+    private List<VideoModel> allVideos;
+    private Context context;
+
+    public VideoAdapter(Context context, List<VideoModel> videoModels){
+        this.allVideos = videoModels;
+        this.context = context;
+    }
+
     @NonNull
     @Override
     public VideoAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -16,19 +35,45 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.MyViewHolder
     }
 
     @Override
-    public void onBindViewHolder(@NonNull VideoAdapter.MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull VideoAdapter.MyViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        holder.title.setText(allVideos.get(position).getTitle());
+        Picasso.get().load(allVideos.get(position).getImageUri()).into(holder.imageView);
+
+        holder.view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("videoData", allVideos.get(position));
+                //v.getContext().startActivity(new Intent(context, Player.class));
+                Intent intent = new Intent(context, Player.class);
+                intent.putExtras(bundle);
+                v.getContext().startActivity(intent);
+            }
+        });
 
     }
 
     @Override
     public int getItemCount() {
-        return 5;
+        return allVideos.size();
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
 
+        ImageView imageView;
+        TextView title, channel, views, days;
+        View view;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            imageView = itemView.findViewById(R.id.videoThumbnail);
+            title = itemView.findViewById(R.id.videoTitle);
+            channel = itemView.findViewById(R.id.videoSource);
+            views = itemView.findViewById(R.id.videoViews);
+            days = itemView.findViewById(R.id.videoPosted);
+            view = itemView;
+
         }
     }
 }
